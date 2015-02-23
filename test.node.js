ff0cc97e8440fae4d3d13c7f20ce44ba4881642c
Ben Briggs <therealbenbriggs@hotmail.com>
2015-02-23 11:07:08 +0000

    Tidy up fixtures, use tape instead of mocha.

--- test.node.js
+++ test.node.js
@@ -2,13 +2,13 @@
 
 var fs     = require('fs'),
     widow  = require('./'),
-    assert = require('assert');
+    test   = require('tape');
 
 var infixture = fs.readFileSync('./fixtures/in.html', 'utf-8');
 var outfixture = fs.readFileSync('./fixtures/out.html', 'utf-8');
 
-describe(require('./package.json').name + ' (using node)', function () {
-    it('should add non breaking spaces to the ends of block level elements', function () {
-        assert.equal(widow(infixture), outfixture);
-    });
+test(require('./package.json').name + ' (in node)', function (t) {
+    var expected = 'should add nbsps to the ends of block level elements';
+    t.plan(1);
+    t.equal(widow(infixture), outfixture, expected);
 });
